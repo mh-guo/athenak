0f87300349f798b2218a9b977ce206ec48fb183b
Minghao Guo <mhguo@princeton.edu>
2026-03-16 17:01:41 -0400

    Adjust the location of radius-dependent density floor parameters in EOS_Data structure

--- src/eos/eos.hpp
+++ src/eos/eos.hpp
@@ -30,9 +30,9 @@ struct EOS_Data {
   bool is_ideal;     // flag to denote ideal gas EOS
   bool use_e, use_t; // use internal energy density (e) or temperature (t) as primitive
   Real dfloor, pfloor, tfloor, sfloor;  // density, pressure, temperature, entropy floors
+  Real rdfloor, rdfloor_rad, rdfloor_pow;  // radius-dependent density floor
   Real gamma_max;    // ceiling on Lorentz factor in SR/GR
   Real sigma_max;    // ceiling on magnetization in MHD
-  Real rdfloor, rdfloor_rad, rdfloor_pow;  // radius-dependent density floor
 
   // IDEAL GAS PRESSURE: converts primitive variable (either internal energy density e
   // or temperature e/d) into pressure.
